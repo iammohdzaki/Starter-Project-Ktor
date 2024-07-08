6edef92d4df9075a190fe480eab731f3cf580ce0
Bitmap <mohdzaki407@gmail.com>
2024-07-08 20:17:54 +0530

    Added Kotlin Lint Workflow

--- src/main/kotlin/com/Application.kt
+++ src/main/kotlin/com/Application.kt
@@ -7,7 +7,7 @@ import com.plugins.*
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+@Suppress("unused") //application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     Locale.init()
     configureKoin()
